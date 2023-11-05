d9087adb3d69984f05c9b61d8ebe68cc5d3817f4
pushedrumex <pushedrumex@naver.com>
2023-11-06 01:33:49 +0900

    14501

---
+++ week-5/pushedrumex/퇴사.py
@@ -0,0 +1,12 @@
+N = int(input())
+schedule = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [0] * (N+2)
+for i in range(N, 0, -1):
+    t, p = schedule[i-1]
+    if i + t - 1 <= N:
+        dp[i] = max(dp[i + t] + p, dp[i + 1])
+    else:
+        dp[i] = dp[i + 1]
+
+print(dp[1])
