9b3b60abc2120bf9769a8c9bf15dfd1212486cb1
sen <64001680+dahyen0o@users.noreply.github.com>
2023-11-24 17:36:51 +0900

    refactor: 순위-검색.py

--- week-6/dahyen0o/순위-검색.py
+++ week-6/dahyen0o/순위-검색.py
@@ -1,29 +1,41 @@
-import re
+from collections import defaultdict
+from itertools import combinations, permutations
 
-def solution(infos, queries):
-    answer = []
-    
-    language = {'java': 0, 'python': 1, 'cpp': 2, '-': 3}
-    part = {'backend': 0, 'frontend': 1, '-': 2}
-    career = {'junior': 0, 'senior': 1, '-': 2}
-    food = {'pizza': 0, 'chicken': 1, '-': 2}
-    
-    count = [[[[[] for _ in range(3)] for _ in range(3)] for _ in range(3)] for _ in range(4)]
+def solution(infos, query):
+    info_score = defaultdict(list)
     
     for info in infos:
-        info = info.split()
-        count[language[info[0]]][part[info[1]]][career[info[2]]][food[info[3]]].append(int(info[4]))
-        count[-1][part[info[1]]][career[info[2]]][food[info[3]]].append(int(info[4]))
-        count[language[info[0]]][-1][career[info[2]]][food[info[3]]].append(int(info[4]))
-        count[language[info[0]]][part[info[1]]][-1][food[info[3]]].append(int(info[4]))
-        count[language[info[0]]][part[info[1]]][career[info[2]]][-1].append(int(info[4]))
-        # ... 실패^^
+        lang, pos, career, food, score = info.split(" ")
+        for l in (lang, '-'):
+            for p in (pos, '-'):
+                for c in (career, '-'):
+                    for f in (food, '-'):
+                        info_score[l + p + c + f].append(int(score))
+    
+    # 점수 리스트 정렬 (이분탐색을 위해)
+    for key in info_score.keys():
+        info_score[key].sort()
+    
+    answer = []
+              
+    for q in query:
+        lang, pos, career, food_score = q.split(' and ')
+        food, score = food_score.split(' ')
         
-    for query in queries:
-        query = re.sub(' and', '', query).split()
-        result = count[language[query[0]]][part[query[1]]][career[query[2]]][food[query[3]]]
+        key = lang + pos + career + food
+        scores = info_score[key]
+        score = int(score)
         
-        print(result)
+        # 이분탐색으로 score 이상인 점수의 개수 구하기
+        left = 0
+        right = len(scores) - 1
 
-    # print(count)
+        while left <= right:
+            mid = (left + right) // 2
+            if scores[mid] >= score:
+                right = mid - 1
+            else:
+                left = mid + 1
+        answer.append(len(scores) - left)
+        
     return answer
