7867c018b5398afeb8ef259938772897de02f1ef
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-11-28 11:41:59 +0900

    8_whatasame (#44)

---
+++ week-8/whatasame/수식 최대화.py
@@ -0,0 +1,24 @@
+# 절대값이 가장 큰 숫자가 우승
+# 절대값 = 우승 금액
+# 우승 금액을 반환
+
+from itertools import permutations
+
+
+def solution(expression):
+    # 모든 우선 순위에 대하여
+    answer = 0
+    for first, second, third in permutations("-+*", 3):
+        second_result = []
+        for second_target in expression.split(third):
+            first_result = []
+            for first_target in second_target.split(second):
+                first_result.append(f"({first_target})")
+
+            second_result.append(f"({second.join(first_result)})")
+
+        result = abs(eval(third.join(second_result)))
+
+        answer = max(answer, result)
+
+    return answer
---
+++ week-8/whatasame/키패드 누르기.py
@@ -0,0 +1,42 @@
+# 1, 4, 7 -> 왼쪽
+# 2, 5, 8, 0, -> 오른쪽
+# 3, 6, 9 -> 둘 중 가까운 것 -> 거리 같다면 주 손 사용
+
+# number 내 중복 번호 존재
+
+keypad = {1: (0, 0), 2: (0, 1), 3: (0, 2),
+          4: (1, 0), 5: (1, 1), 6: (1, 2),
+          7: (2, 0), 8: (2, 1), 9: (2, 2),
+          "*": (3, 0), 0: (3, 1), "#": (3, 2)}
+
+
+def solution(numbers, hand):
+    # 손가락 초기화
+    left, right = keypad["*"], keypad["#"]
+
+    answer = []
+    for number in numbers:
+        left, right, used = move(number, left, right, hand)
+        answer.append(used)
+
+    return "".join(map(str, answer))
+
+
+def move(number, left, right, hand):
+    if number in (1, 4, 7):
+        return keypad[number], right, "L"
+    if number in (3, 6, 9):
+        return left, keypad[number], "R"
+
+    left_distance = abs(keypad[number][0] - left[0]) + abs(keypad[number][1] - left[1])
+    right_distance = abs(keypad[number][0] - right[0]) + abs(keypad[number][1] - right[1])
+
+    if left_distance < right_distance:
+        return keypad[number], right, "L"
+    if left_distance > right_distance:
+        return left, keypad[number], "R"
+    else:
+        if hand == "left":
+            return keypad[number], right, "L"
+        elif hand == "right":
+            return left, keypad[number], "R"
