d860cbbf26dc0698a871202677fb575aa966c9e0
whatasame <97666463+whatasame@users.noreply.github.com>
2023-12-11 22:22:12 +0900

    feat(wrong): BOJ 배열 돌리기 5 - 새로운 접근 방법
    [반례]
    input:
    4 6 49
    3 2 6 3 1 2
    9 7 8 2 1 4
    5 9 2 1 9 6
    2 1 3 8 6 3
    3 1 4 2 6 5 1 4 2 6 3 5 2 6 1 5 4 3 1 6 4 2 5 3 1 6 4 2 5 3 1 6 4 2 5 3 1 6 4 2 5 3 1 6 4 2 5 3 1
    
    output:
    2 1 3 6 2 3
    4 1 2 8 7 9
    6 9 1 2 9 5
    3 6 8 3 1 2

--- week-10/whatasame/배열 돌리기 5.py
+++ week-10/whatasame/배열 돌리기 5.py
@@ -1,11 +1,11 @@
 # n, m은 짝수
 
 """
-4 2 1
-1 6
-7 2
-1 8
-7 4
+4 6 2
+10 11 12 20 21 22
+13 14 15 23 24 25
+30 31 32 40 41 42
+33 34 35 43 44 45
 ?
 """
 
@@ -20,11 +20,11 @@ operations = list(map(int, input().split()))
 def solution(n, m, r, arr, operations):
     functions = [None, first, second, third, fourth, fifth, sixth]
 
-    vertical, horizontal, angle = False, False, 0
-    positions = [0, 1, 2, 3]
+    # 부분그룹번호, 상하반전, 좌우반전, 오른쪽회전
+    sections = [[num, False, False, 0] for num in range(4)]
 
     for operation in operations:
-        vertical, horizontal, angle, positions = functions[operation](vertical, horizontal, angle, positions)
+        sections = functions[operation](sections)
 
     # 원본 배열 0, 1, 2, 3으로 쪼개기
     zero = [[col for col in row[:m // 2]] for row in arr[:n // 2]]
@@ -33,31 +33,36 @@ def solution(n, m, r, arr, operations):
     three = [[col for col in row[m // 2:]] for row in arr[n // 2:]]
     divided = [zero, one, two, three]
 
-    # 서브배열 회전 시키기
-    divided = [rotate(divide, angle) for divide in divided]
+    for i in range(4):
+        num, vertical, horizontal, angle = sections[i]
+
+        # 서브배열 상하반전
+        if vertical:
+            divided[num] = divided[num][::-1]
+
+        # 서브배열 좌우반전
+        if horizontal:
+            divided[num] = [row[::-1] for row in divided[num]]
+
+        # 서브배열 회전 시키기
+        divided[num] = rotate(divided[num], angle)
 
     # 원래대로 합치기
     new_arr = []
-    for left, right in zip(divided[positions[0]], divided[positions[1]]):
+    for left, right in zip(divided[sections[0][0]], divided[sections[1][0]]):
         left.extend(right)
         new_arr.append(left)
-    for left, right in zip(divided[positions[2]], divided[positions[3]]):
+    for left, right in zip(divided[sections[2][0]], divided[sections[3][0]]):
         left.extend(right)
         new_arr.append(left)
-
-    # 상하반전, 좌우반전 진행
-    if vertical:
-        new_arr = new_arr[::-1]
-    if horizontal:
-        new_arr = [row[::-1] for row in new_arr]
 
     # 최종 배열 반환
     return new_arr
 
 
 # 오른쪽으로 90도 서브배열 n번 회전
-def rotate(arr, n):
-    for _ in range(n):
+def rotate(arr, cnt):
+    for _ in range(cnt):
         n, m = len(arr), len(arr[0])
         new_arr = [[0 for _ in range(n)] for _ in range(m)]
 
@@ -71,35 +76,63 @@ def rotate(arr, n):
 
 
 # 상하반전
-def first(vertical, horizontal, angle, pos):
-    return not vertical, horizontal, angle, pos
+def first(sections):
+    return [
+        [sections[2][0], not sections[2][1], sections[2][2], sections[2][3]],
+        [sections[3][0], not sections[3][1], sections[3][2], sections[3][3]],
+        [sections[0][0], not sections[0][1], sections[0][2], sections[0][3]],
+        [sections[1][0], not sections[1][1], sections[1][2], sections[1][3]],
+    ]
 
 
 # 좌우반전
-def second(vertical, horizontal, angle, pos):
-    return vertical, not horizontal, angle, pos
+def second(sections):
+    return [
+        [sections[1][0], sections[1][1], not sections[1][2], sections[1][3]],
+        [sections[0][0], sections[0][1], not sections[0][2], sections[0][3]],
+        [sections[3][0], sections[3][1], not sections[3][2], sections[3][3]],
+        [sections[2][0], sections[2][1], not sections[2][2], sections[2][3]],
+    ]
 
 
 # 오른쪽 90도
-def third(vertical, horizontal, angle, pos):
-    new_angle = (angle + 1) % 4
-    return vertical, horizontal, new_angle, [pos[2], pos[0], pos[3], pos[1]]
+def third(sections):
+    return [
+        [sections[2][0], sections[2][1], sections[2][2], (sections[2][3] + 1) % 4],
+        [sections[0][0], sections[0][1], sections[0][2], (sections[0][3] + 1) % 4],
+        [sections[3][0], sections[3][1], sections[3][2], (sections[3][3] + 1) % 4],
+        [sections[1][0], sections[1][1], sections[1][2], (sections[1][3] + 1) % 4],
+    ]
 
 
 # 왼쪽 90도
-def fourth(vertical, horizontal, angle, pos):
-    new_angle = (angle - 1) % 4
-    return vertical, horizontal, new_angle, [pos[1], pos[3], pos[0], pos[2]]
+def fourth(sections):
+    return [
+        [sections[1][0], sections[1][1], sections[1][2], (sections[1][3] - 1) % 4],
+        [sections[3][0], sections[3][1], sections[3][2], (sections[3][3] - 1) % 4],
+        [sections[0][0], sections[0][1], sections[0][2], (sections[0][3] - 1) % 4],
+        [sections[2][0], sections[2][1], sections[2][2], (sections[2][3] - 1) % 4],
+    ]
 
 
 # 4사분면 오른쪽 90도
-def fifth(vertical, horizontal, angle, pos):
-    return vertical, horizontal, angle, [pos[2], pos[0], pos[3], pos[1]]
+def fifth(sections):
+    return [
+        [sections[2][0], sections[2][1], sections[2][2], sections[2][3]],
+        [sections[0][0], sections[0][1], sections[0][2], sections[0][3]],
+        [sections[3][0], sections[3][1], sections[3][2], sections[3][3]],
+        [sections[1][0], sections[1][1], sections[1][2], sections[1][3]],
+    ]
 
 
 # 4사분면 왼쪽 90도
-def sixth(vertical, horizontal, angle, pos):
-    return vertical, horizontal, angle, [pos[1], pos[3], pos[0], pos[2]]
+def sixth(sections):
+    return [
+        [sections[1][0], sections[1][1], sections[1][2], sections[1][3]],
+        [sections[3][0], sections[3][1], sections[3][2], sections[3][3]],
+        [sections[0][0], sections[0][1], sections[0][2], sections[0][3]],
+        [sections[2][0], sections[2][1], sections[2][2], sections[2][3]],
+    ]
 
 
 answer = solution(n, m, r, arr, operations)
