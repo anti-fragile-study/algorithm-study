f78d988e04784e7fdcb31bf5904870a5bca2a28b
sen <64001680+dahyen0o@users.noreply.github.com>
2023-12-19 11:12:52 +0900

    feat: 사라지는-발판.java (실패)

---
+++ week-11/dahyen0o/사라지는-발판.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    
+    private static final int[] dr = {0, +1, 0, -1};
+    private static final int[] dc = {+1, 0, -1, 0};
+    
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        play(1, board, aloc, bloc);
+        int answer = -1;
+        return answer;
+    }
+    
+    private void play(int round, int[][] board, int[] me, int[] you) {
+        int result = 0;
+        
+    }
+}
