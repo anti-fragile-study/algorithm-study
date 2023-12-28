798bb4be8fa6c86975482e72af1ffc495bc59581
sen <64001680+dahyen0o@users.noreply.github.com>
2023-12-28 14:16:09 +0900

    feat: 카드-짝-맞추기.py
    완전탐색, BFS
    (실패 이유: BFS 시작할 때 설정을 잘못함)

--- week-12/dahyen0o/카드-짝-맞추기.py
+++ week-12/dahyen0o/카드-짝-맞추기.py
@@ -16,8 +16,8 @@ def solution(board, r, c):
     answer = 1_000_000_000
     # 맞출 카드 순서를 정해 게임 플레이
     for cards in permutations(range(1, max_card + 1)):
-        # print(cards, play(board, r, c, cards))
         answer = min(answer, play(board, r, c, cards))
+        
     return answer
 
 def play(board, r, c, cards):
@@ -27,28 +27,24 @@ def play(board, r, c, cards):
         # 현재 위치에서 card 두 개를 제거
         r, c, count = open_cards(board, card, r, c)
         game_count += count + 2 # enter 키 입력 2번 추가
-        print(cards, count + 2, "위치", r, c)
     return game_count
         
 def open_cards(board, card, r, c):
     # BFS로 두 card를 없애는 최단 경로 탐색    
-    visited = [[[False for _ in range(3)] for _ in range(4)] for _ in range(4)]
+    visited = [[[False for _ in range(4)] for _ in range(4)] for _ in range(4)]
     visited[r][c][0] = True # row, col, 카드를 연 횟수
+    
     queue = deque([(r, c, 0, 0)]) # row, col, 카드를 연 횟수, 키 조작 횟수
     if board[r][c] == card:
         visited[r][c][cards_coord[card].index((r, c)) + 1] = True
         queue.popleft()
-        queue.append((r, c, cards_coord[card].index((r, c)) + 1, 1))
+        queue.append((r, c, cards_coord[card].index((r, c)) + 1, 0))
     
     while queue:
         curr = queue.popleft()
-        
-        # if board[curr[0]][curr[1]] == card: # 현재 카드가 card임
-        #     print(card, curr)
             
         if curr[2] == 3: # 두 개의 카드를 열었음
             # 맞춘 카드들 없애기
-            # print(card, card_coord)
             for coord in cards_coord[card]:
                 board[coord[0]][coord[1]] = 0
             return (curr[0], curr[1], curr[3])
@@ -61,8 +57,7 @@ def open_cards(board, card, r, c):
             next = (r, c, curr[2], curr[3] + 1)
             if board[r][c] == card:
                 next = (r, c, curr[2] + cards_coord[card].index((r, c)) + 1, curr[3] + 1)
-                visited[r][c][cards_coord[card].index((r, c)) + 1] = True
-                # print((r,c), cards_coord[card].index((r, c)) + 1)
+                visited[r][c][next[2]] = True
             visited[r][c][curr[2]] = True
             queue.append(next)
         
@@ -80,11 +75,9 @@ def open_cards(board, card, r, c):
             next = (r, c, curr[2], curr[3] + 1)
             if board[r][c] == card:
                 next = (r, c, curr[2] + cards_coord[card].index((r, c)) + 1, curr[3] + 1)
-                visited[r][c][cards_coord[card].index((r, c)) + 1] = True
-                # print((r,c), cards_coord[card].index((r, c)) + 1)
+                visited[r][c][next[2]] = True
             visited[r][c][curr[2]] = True
             queue.append(next)
-            
 
 def can_move(r, c):
     return r >= 0 and c >= 0 and r < 4 and c < 4
