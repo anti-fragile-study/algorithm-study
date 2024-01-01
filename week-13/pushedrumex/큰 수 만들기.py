c55cb67b253c30c4993bfa5053f37bd9a1a78f07
pushedrumex <pushedrumex@naver.com>
2024-01-02 00:17:58 +0900

    큰 수 만들기

---
+++ week-13/pushedrumex/큰 수 만들기.py
@@ -0,0 +1,9 @@
+def solution(number, k):
+    q = []
+    for n in number:
+        while q and k > 0 and n > q[-1]:
+            k -= 1
+            q.pop()
+        q.append(n)
+        
+    return "".join(q[:len(number) - k])
