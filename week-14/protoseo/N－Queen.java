c35f34753e293e18203f4fe0c2f31df4e3d9e320
protoseo <proto_seo@naver.com>
2024-01-03 08:16:30 +0000

    프로그래머스 N－Queen 문제 풀이

--- week-14/protoseo/N－Queen.java
+++ week-14/protoseo/N－Queen.java
@@ -1,56 +1,6 @@
 class Solution {
-    
-    int[][] isVisited;
-    int answer;
-    int[] dx = {0, 1, 0, -1, 1, 1, -1, -1};
-    int[] dy = {1, 0, -1, 0, -1, 1, -1, 1};
-    
     public int solution(int n) {
-        isVisited = new int[n][n];
-        find(0, n);
+        int answer = 0;
         return answer;
-    }
-    
-    private void find(int y, int n) {
-        if (y == n) {
-            answer++;
-            return;
-        }
-        for (int x = 0; x < n; x++) {
-            if (isVisited[y][x] > 0) {
-                continue;
-            }
-            visit(y, x, n);
-            find(y + 1, n);
-            unvisit(y, x, n);
-        }
-    }
-    
-    private void visit(int y, int x, int n) {
-        isVisited[y][x]++;
-        for (int k = 0; k < 8; k++) {
-            int xx = x + dx[k];
-            int yy = y + dy[k];
-            
-            while (0 <= xx && xx < n && 0 <= yy && yy < n) {
-                isVisited[yy][xx]++;
-                xx += dx[k];
-                yy += dy[k];
-            }
-        }
-    }
-    
-    private void unvisit(int y, int x, int n) {
-        isVisited[y][x]--;
-        for (int k = 0; k < 8; k++) {
-            int xx = x + dx[k];
-            int yy = y + dy[k];
-            
-            while (0 <= xx && xx < n && 0 <= yy && yy < n) {
-                isVisited[yy][xx]--;
-                xx += dx[k];
-                yy += dy[k];
-            }
-        }
     }
 }
