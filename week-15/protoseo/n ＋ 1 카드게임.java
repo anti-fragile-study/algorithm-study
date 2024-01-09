da95f551153cc04d551992c5b226d80f649529e9
protoseo <proto_seo@naver.com>
2024-01-09 15:52:41 +0000

    프로그래머스 n ＋ 1 카드게임 문제 풀이

--- week-15/protoseo/n ＋ 1 카드게임.java
+++ week-15/protoseo/n ＋ 1 카드게임.java
@@ -1,6 +1,71 @@
 class Solution {
+    int n;
+    boolean[] hands;
+    boolean[] candidate;
     public int solution(int coin, int[] cards) {
-        int answer = 0;
-        return answer;
+        n = cards.length;
+        hands = new boolean[n + 1];
+        candidate = new boolean[n + 1];
+        for (int i = 0; i < n / 3; i++) {
+           hands[cards[i]] = true;
+        }
+        
+        int round = 0;
+        for (int idx = n / 3; coin >= 0 && idx + 1 <= n; idx += 2, round++) {
+            if (!hasCardInHand() && coin <= 0) {
+                break;
+            }
+            candidate[cards[idx]] = true;
+            candidate[cards[idx + 1]] = true;
+            if (!canGoNextRound(coin)) {
+                break;
+            }
+            coin = exchange(coin);
+        }
+        return round + 1;
+    }
+    
+    private boolean hasCardInHand() {
+        for (int i = 1; i <= n; i++) {
+            if (hands[i]) {
+                return true;
+            }
+        }
+        return false;
+	}
+
+    private boolean canGoNextRound(int coin) {
+        boolean result = false;
+        for (int i = 1; i <= n; i++) {
+            result |= hands[i] && hands[n + 1 - i];
+            result |= coin >= 1 && hands[i] && candidate[n + 1 - i];
+            result |= coin >= 2 && candidate[i] && candidate[n + 1 - i];
+        }
+        return result;
+    }
+
+    private int exchange(int coin) {
+        for (int i = 1; i <= n / 2; i++) {
+            if (hands[i] && hands[n + 1 - i]) {
+                hands[i] = false;
+                hands[n + 1 - i] = false;
+                return coin;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (coin >= 1 && hands[i] && candidate[n + 1 - i]) {
+                hands[i] = false;
+                candidate[n + 1 - i] = false;
+                return coin - 1;
+            }
+        }
+        for (int i = 1; i <= n / 2; i++) {
+            if (coin >= 2 && candidate[i] && candidate[n + 1 - i]) {
+                candidate[i] = false;
+                candidate[n + 1 - i] = false;
+                return coin - 2;
+            }
+        }
+        return -1;
     }
 }
