dc73f64b5b3fe90cb67ab7fa24b045c92f4743f7
whatasame <97666463+whatasame@users.noreply.github.com>
2024-02-20 10:20:38 +0900

    feat(fail): BOJ 회전 미로 탐색

---
+++ week-20/whatasame/회전 미로 탐색.py
@@ -0,0 +1,16 @@
+# S에서 E까지 최소 이동 시간을 반환
+
+# 1초마다 가만히 있거나 상하좌우로 이동할 수 있다
+# 이후 현재 구역이 시계방향으로 90도 회전, 다른 구역은 정위치
+
+k = int(input())
+board = [list(input()) for _ in range(4 * k)]
+
+def solution(board):
+    # S와 E의 좌표를 찾는다: n^2
+    pass
+
+def rotate(arr):
+    pass
+
+print(solution(board))
