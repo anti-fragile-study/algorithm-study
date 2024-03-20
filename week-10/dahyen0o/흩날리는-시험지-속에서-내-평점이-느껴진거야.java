d5bd4ca912b8415ab3047a01e285972cb804b048
sen <seeunchoi99@gmail.com>
2024-03-20 16:34:06 +0900

    feat: 흩날리는-시험지-속에서-내-평점이-느껴진거야.java
    이분 탐색

--- week-10/dahyen0o/흩날리는-시험지-속에서-내-평점이-느껴진거야.java
+++ week-10/dahyen0o/흩날리는-시험지-속에서-내-평점이-느껴진거야.java
@@ -5,7 +5,7 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ17951 {
+public class Main {
     static BufferedReader br;
     static BufferedWriter bw;
     static StringTokenizer st;
@@ -21,37 +21,45 @@ public class BOJ17951 {
         final int K = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        final int[] scores = new int[N + 1];
-
-        // 누적 합 계산
-        for (int i = 1; i < scores.length; i++) {
-            scores[i] = scores[i - 1];
-            scores[i] += Integer.parseInt(st.nextToken());
+        final int[] scores = new int[N];
+        int MAX_SCORE = 0;
+        for (int n = 0; n < scores.length; n++) {
+            scores[n] = Integer.parseInt(st.nextToken());
+            MAX_SCORE += scores[n];
         }
 
-        final double avg = scores[N] / (double) K;
+        int left = 0, right = MAX_SCORE;
+        int answer = 0;
 
-        // 평균 값과 최대한 가깝게 맞추기
-        int minSum = Integer.MAX_VALUE;
-        int start = 1;
-        for (int end = 1; end < scores.length; end++) {
-            final int sum = scores[end] - scores[start - 1];
-            if (sum > avg) {
-                final int preSum = scores[end - 1] - scores[start - 1];
-                if (Math.abs(preSum - avg) < Math.abs(sum - avg) && start < end) {
-                    start = end;
-                    minSum = Math.min(minSum, preSum);
-                } else {
-                    start = end + 1;
-                    minSum = Math.min(minSum, sum);
-                }
+        while (left <= right) {
+            int minScore = (left + right) / 2;
+            
+            // K개의 그룹으로 쪼갰을 때 합이 모두 minScore 넘을 수 있을지 검사
+            if (canLowerBound(minScore, scores, K)) {
+                answer = Math.max(answer, minScore);
+                left = minScore + 1;
+            } else {
+                right = minScore - 1;
+            }
+        }
+        
+        bw.write(sb.append(answer).append("\n").toString());
+        bw.flush();
+    }
+
+    private static boolean canLowerBound(final int lowerBound, final int[] scores, final int K) {
+        int k = 0;
+        int score = 0;
+
+        for (int i = 0; i < scores.length; i++) {
+            score += scores[i];
+
+            if (score >= lowerBound) {
+                score = 0;
+                k++;
             }
         }
 
-        minSum = Math.min(minSum, scores[scores.length - 1] - scores[start - 1]);
-        
-        sb.append(minSum).append("\n");
-        bw.write(sb.toString());
-        bw.flush();
+        return k >= K;
     }
 }
