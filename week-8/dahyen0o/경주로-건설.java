e7fce2cb27c7ede3f18f2a953ca6edf56ef19cec
sen <seeunchoi99@gmail.com>
2024-03-27 12:59:13 +0900

    feat: 경주로-건설.java
    가중치 bfs

--- week-8/dahyen0o/경주로-건설.java
+++ week-8/dahyen0o/경주로-건설.java
@@ -2,57 +2,57 @@ import java.util.*;
 
 class Solution {
     
-    static class Point {
-        int r, c;
-        int dir;
-        
-        Point(int r, int c, int dir) {
-            this.r = r;
-            this.c = c;
-            this.dir = dir;
-        }
-    }
+    private static final int[] dr = {-1, 0, +1, 0};
+    private static final int[] dc = {0, +1, 0, -1};
+    private static final int MAX = Integer.MAX_VALUE;
     
-    private static final int[] dr = {0, +1, 0, -1};
-    private static final int[] dc = {+1, 0, -1, 0};
+    private int N;
+    private int[][] board;
+    private int[][][] minCosts;
     
     public int solution(final int[][] board) {
-        for(int r = 0; r < board.length; r++) {
-            for(int c = 0; c < board[r].length; c++) {
-                System.out.printf("%2d ", board[r][c]);
-            }
-            System.out.println();
+        this.board = board;
+        this.N = board.length;
+        this.minCosts = new int[N][N][2];
+        for(int r = 0; r < N; r++) {
+            for(int c = 0; c < N; c++)
+                Arrays.fill(minCosts[r][c], MAX);
         }
-        final int[][] costs = new int[board.length][board.length];
-        for(int[] cost : costs) {
-            Arrays.fill(cost, Integer.MAX_VALUE);
-        }
-        costs[0][0] = 0;
-                
-        final Queue<Point> queue = new ArrayDeque<>();
-        queue.add(new Point(0, 0, -1));
+        
+        return minCost();
+    }
+    
+    private int minCost() {
+        Arrays.fill(minCosts[0][0], 0);
+        
+        final Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] {0, 0, -1, 0}); // (r, c, dir, cost)
         
         while(!queue.isEmpty()) {
-            final Point curr = queue.poll();
-            
-            if(curr.r == board.length - 1 && curr.c == board.length - 1) {
-                continue;
-            }
+            final int[] curr = queue.poll();
             
             for(int dir = 0; dir < 4; dir++) {
-                final int r = curr.r + dr[dir];
-                final int c = curr.c + dc[dir];
-                if(r < 0 || c < 0 || r >= board.length || c >= board.length) continue;
-                if(board[r][c] == 1) continue;
+                final int r = curr[0] + dr[dir];
+                final int c = curr[1] + dc[dir];
+                if(!inBound(r, c) || board[r][c] == 1) continue;
                 
-                final int cost = (curr.dir < 0 || curr.dir == dir) ? 100 : 600;
-                if(costs[r][c] >= costs[curr.r][curr.c] + cost) {
-                    costs[r][c] = costs[curr.r][curr.c] + cost;
-                    queue.add(new Point(r, c, dir));
+                final int cost = curr[3] + 100 + (isEdge(curr[2], dir) ? 500 : 0);
+                if(minCosts[r][c][dir % 2] > cost) {
+                    minCosts[r][c][dir % 2] = cost;
+                    queue.add(new int[] {r, c, dir, cost});
                 }
             }
         }
         
-        return costs[board.length - 1][board.length - 1];
+        return Math.min(minCosts[N - 1][N - 1][0], minCosts[N - 1][N - 1][1]);
+    }
+    
+    private boolean inBound(final int r, final int c) {
+        return r >= 0 && c >= 0 && r < N && c < N;
+    }
+    
+    private boolean isEdge(final int d1, final int d2) {
+        if(d1 < 0 || d2 < 0) return false;
+        return ((d1 ^ d2) & 1) > 0;
     }
 }
