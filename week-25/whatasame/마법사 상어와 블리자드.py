65353b184cab9000ac3924e0966ad8eb9606bc5e
whatasame <97666463+whatasame@users.noreply.github.com>
2024-04-02 16:31:15 +0900

    feat: BOJ 마법사 상어와 블리자드

--- week-25/whatasame/마법사 상어와 블리자드.py
+++ week-25/whatasame/마법사 상어와 블리자드.py
@@ -9,13 +9,25 @@
 
 # 구슬이 이동하는 건 리스트로 이동시키고 리스트를 다시 배열로 바꾸는 방식은 어떨까
 
+"""
+3 1
+0 0 0
+0 0 0
+0 0 0
+1 1
+-> 0
+"""
+
 import sys
+from collections import deque
 
 input = lambda: sys.stdin.readline().strip()
 
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-blizzards = [tuple(map(int, input().split())) for _ in range(m)]
+a = [list(map(int, input().split())) for _ in range(n)]
+b = [tuple(map(int, input().split())) for _ in range(m)]
+
+directions = {1: (-1, 0, 4), 2: (1, 0, 3), 3: (0, -1, 1), 4: (0, 1, 2)} # (dr, dc, next_direction)
 
 
 def solution(arr, blizzards):
@@ -28,29 +40,26 @@ def solution(arr, blizzards):
     # 각 블리자드에 대하여
     for d, s in blizzards:
         # 블리자드 사용
-        if d == 1:  # 상
-            for r in range(center - 1, center - 1 - s, -1):
-                arr[r][center] = 0
-        if d == 2:  # 하
-            for r in range(center + 1, center + 1 + s):
-                arr[r][center] = 0
-        if d == 3:  # 좌
-            for c in range(center - 1, center - 1 - s, -1):
-                arr[center][c] = 0
-        if d == 4:  # 우
-            for c in range(center + 1, center + 1 + s):
-                arr[center][c] = 0
+        r, c = center, center
+        dr, dc, _ = directions[d]
+        for _ in range(s):
+            r, c = r + dr, c + dc
+            arr[r][c] = 0
 
         # 리스트로 변환
-        line = to_line(arr) # 밖에서 안으로
+        beads = to_beads(arr)  # 밖에서 안으로
 
         # 연속하는 구슬 폭발
-        line = explode(answer, line)
+        beads = explode(answer, beads)
 
         # 구슬 변화
         reversed_line = []  # 안에서 밖으로
-        num, count = line[-1], 0
-        for idx, val in enumerate(line[::-1]):
+        num, count = None, 0
+        for idx, val in enumerate(beads[::-1]):
+            if num is None:
+                num, count = val, 1
+                continue
+
             if val != num:
                 reversed_line.append(count)
                 reversed_line.append(num)
@@ -60,7 +69,7 @@ def solution(arr, blizzards):
         reversed_line.append(count)
         reversed_line.append(num)
 
-        reversed_line = [-1] + reversed_line # 상어 붙여주기
+        reversed_line = [-1] + reversed_line  # 상어 붙여주기
         reversed_line = reversed_line[:n ** 2]  # 길이 제한
         reversed_line = reversed_line + [0] * (n ** 2 - len(reversed_line))  # 부족한 개수는 0으로 채워넣기
 
@@ -70,44 +79,36 @@ def solution(arr, blizzards):
     return answer[1] + 2 * answer[2] + 3 * answer[3]
 
 
-def to_line(arr):
+def to_beads(arr):
+    beads = deque()
     r, c = 0, 0
 
-    direction = 1  # 우하좌상 = 1234
-    line = []
-    while arr[r][c] != -1: # 상어 제외
+    dr, dc, nd = directions[4] # 오른쪽을 향해 이동
+    while arr[r][c] != -1:  # 상어 제외
         # 값 추가
-        line.append(arr[r][c])
-        arr[r][c] = None
+        if arr[r][c] > 0:
+            beads.append(arr[r][c])
+        arr[r][c] = None # 방문 처리
 
         # out of idx이거나 방문한 곳이라면 방향 전환
-        if direction == 1 and (c + 1 >= n or arr[r][c + 1] is None):
-            direction = 2
-        if direction == 2 and (r + 1 >= n or arr[r + 1][c] is None):
-            direction = 3
-        if direction == 3 and (c - 1 < 0 or arr[r][c - 1] is None):
-            direction = 4
-        if direction == 4 and (r - 1 < 0 or arr[r - 1][c] is None):
-            direction = 1
+        if not (0 <= r + dr < n and 0 <= c + dc < n) or arr[r + dr][c + dc] is None:
+            dr, dc, nd = directions[nd]
 
         # 이동
-        if direction == 1:
-            c += 1
-        if direction == 2:
-            r += 1
-        if direction == 3:
-            c -= 1
-        if direction == 4:
-            r -= 1
+        r, c = r + dr, c + dc
 
-    return [val for val in line if val != 0] # 블리자드에서 없어진 빈 칸 제거
+    return beads
+
 
 def explode(answer, line):
     # 연속되는 그룹 찾기
     exploded = False
-    num, indices = line[0], []
+    num, indices = None, []
     for idx, val in enumerate(line):
         # 다른 숫자거나 마지막 원소일 때 그룹 구분
+        if num is None:
+            num, indices = val, [idx]
+            continue
         if val != num:
             # 조건에 맞으면 폭발
             if len(indices) >= 4:
@@ -134,34 +135,24 @@ def explode(answer, line):
     # 재귀적으로 수행
     return explode(answer, line)
 
-def to_arr(line):
+
+def to_arr(beads):
+    r, c = 0, 0
     arr = [[None for _ in range(n)] for _ in range(n)]
 
-    r, c = 0, 0
-    direction = 1  # 우하좌상 = 1234
-    for val in line:
+    dr, dc, nd = directions[4] # 오른쪽을 향해 이동
+    for val in beads:
+        # 값 추가
         arr[r][c] = val
 
         # out of idx이거나 방문한 곳이라면 방향 전환
-        if direction == 1 and (c + 1 >= n or arr[r][c + 1] is not None):
-            direction = 2
-        if direction == 2 and (r + 1 >= n or arr[r + 1][c] is not None):
-            direction = 3
-        if direction == 3 and (c - 1 < 0 or arr[r][c - 1] is not None):
-            direction = 4
-        if direction == 4 and (r - 1 < 0 or arr[r - 1][c] is not None):
-            direction = 1
+        if not (0 <= r + dr < n and 0 <= c + dc < n) or arr[r + dr][c + dc] is not None:
+            dr, dc, nd = directions[nd]
 
         # 이동
-        if direction == 1:
-            c += 1
-        if direction == 2:
-            r += 1
-        if direction == 3:
-            c -= 1
-        if direction == 4:
-            r -= 1
+        r, c = r + dr, c + dc
 
     return arr
 
-print(solution(arr, blizzards))
+
+print(solution(a, b))
