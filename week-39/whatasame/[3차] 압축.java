345a63a488bd225e64a03f72fa08df992f404418
whatasame <97666463+whatasame@users.noreply.github.com>
2024-09-24 11:17:31 +0900

    feat: Programmers [3차] 압축.java

---
+++ week-39/whatasame/[3차] 압축.java
@@ -0,0 +1,40 @@
+// 무손실 알고리즘
+// 입력은 모두 대문자
+
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(String msg) {
+        Map<String, Integer> dict = new HashMap<>();
+        String[] capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+        for (int idx = 0; idx < capitals.length; idx++) {
+            dict.put(capitals[idx], idx + 1);
+        }
+
+        int nxt = capitals.length + 1; // 27
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < msg.length(); i++) {
+            String sub = null;
+            for (int j = i; j < msg.length(); j++) {
+                sub = msg.substring(i, j + 1);  // i to j
+
+                if (!dict.containsKey(sub)) {
+                    dict.put(sub, nxt++);
+                    String prev = msg.substring(i, j); // i to j - 1 = sub - 1
+                    answer.add(dict.get(prev));
+
+                    i += prev.length() - 1;
+                    sub = null;
+                    break;
+                }
+            }
+            if (sub != null) {
+                answer.add(dict.get(sub));
+                i += sub.length() - 1;
+            }
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
